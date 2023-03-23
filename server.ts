60456f768ad70a0390b7c9296f27921e23b71dbe
Nikolay Smk <nikolaysmyk@icloud.com>
2023-03-24 00:48:42 +0300

    init

--- server.ts
+++ server.ts
@@ -7,9 +7,17 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send(product);
 });
 
 app.listen(PORT, () => {
   console.log(`Express with Typescript! http://localhost:${PORT}`);
 });
+
+// show dataset product in array
+const product = [
+  {
+    title: "first",
+    price: 1000,
+  },
+];
